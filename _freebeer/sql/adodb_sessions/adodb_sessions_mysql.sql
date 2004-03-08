e24da1e00ca74ff4c0863c530c4d87664a26dd38
rossta <>
2004-03-08 19:58:12 +0000

    Fixed add_headers.php mistake

--- _freebeer/sql/adodb_sessions/adodb_sessions_mysql.sql
+++ _freebeer/sql/adodb_sessions/adodb_sessions_mysql.sql
@@ -5,16 +5,16 @@
 
 DROP TABLE IF EXISTS state;
 CREATE TABLE IF NOT EXISTS state (
-	id	CHAR(2)		NOT NULL AUTO_INCREMENT	COMMENT 'State ID',
-	name	CHAR(20)	NOT NULL COMMENT 'State name',
+	id	CHAR(2)		NOT NULL AUTO_INCREMENT, -- COMMENT 'State ID',
+	name	CHAR(20)	NOT NULL, -- COMMENT 'State name',
 	PRIMARY KEY (id),
 	UNIQUE INDEX ux_name (name)
 );
 
 DROP TABLE IF EXISTS event_type;
 CREATE TABLE IF NOT EXISTS event_type (
-	id	INT(11)		NOT NULL AUTO_INCREMENT	COMMENT 'Event type ID',
-	name	CHAR(40)	NOT NULL COMMENT 'Event type name',
+	id	INT(11)		NOT NULL AUTO_INCREMENT, -- COMMENT 'Event type ID',
+	name	CHAR(40)	NOT NULL, -- COMMENT 'Event type name',
 	PRIMARY KEY (id),
 	UNIQUE INDEX ux_name (name)
 );
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS event_type (
 DROP TABLE IF EXISTS location;
 
 CREATE TABLE IF NOT EXISTS location (
-	id		INT(11)		NOT NULL AUTO_INCREMENT	COMMENT 'Event type ID',
+	id		INT(11)		NOT NULL AUTO_INCREMENT, -- COMMENT 'Event type ID',
 	address		VARCHAR(40)	NOT NULL,
 	address2	VARCHAR(40)	NOT NULL,
 	city    	VARCHAR(40)	NOT NULL,
@@ -37,12 +37,12 @@ CREATE TABLE IF NOT EXISTS location (
 
 DROP TABLE IF EXISTS event;
 CREATE TABLE IF NOT EXISTS event (
-	id		INT(11)		NOT NULL AUTO_INCREMENT	COMMENT 'Event ID',
-	user_id		INT(11)		NOT NULL COMMENT 'User ID of host',
-	event_name	CHAR(80)	NOT NULL COMMENT 'Event name',
-	poker_game_id	INT(11)		NOT NULL COMMENT '',
-	event_type_id	INT(11)		NOT NULL COMMENT '',
-	location_id	INT(11)		NOT NULL COMMENT '',
+	id		INT(11)		NOT NULL AUTO_INCREMENT, -- COMMENT 'Event ID',
+	user_id		INT(11)		NOT NULL, -- COMMENT 'User ID of host',
+	event_name	CHAR(80)	NOT NULL, -- COMMENT 'Event name',
+	poker_game_id	INT(11)		NOT NULL, -- COMMENT '',
+	event_type_id	INT(11)		NOT NULL, -- COMMENT '',
+	location_id	INT(11)		NOT NULL, -- COMMENT '',
 	PRIMARY KEY	(id),
 	INDEX		(user_id),
 	INDEX		(event_name),	// should this be full text indexed?
