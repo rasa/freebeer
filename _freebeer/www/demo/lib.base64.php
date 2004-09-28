401527a8f85abd0085bbcc35812ee3b8905c470a
rossta <>
2004-09-28 01:48:29 +0000

    *** empty log message ***

--- _freebeer/www/demo/lib.base64.php
+++ _freebeer/www/demo/lib.base64.php
@@ -22,7 +22,7 @@ if (!isset($_REQUEST['t_base64decode'])) {
 }
 
 ?>
-<script type="text/javascript" language="javascript1.2" src="/lib/Base64.js">
+<script type="text/javascript" language="javascript1.2" src="../lib/Base64.js">
 </script>
 <script type="text/javascript" language="javascript1.2">
 <!-- // <![CDATA[
